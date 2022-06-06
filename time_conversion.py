4484895f720415ed4040da41d851a926d280643b
abel-arismendy <abel@arismendy.co>
2022-06-05 22:54:16 -0500

    Time Conversion

--- time_conversion.py
+++ time_conversion.py
@@ -14,16 +14,26 @@ import sys
 #
 
 def timeConversion(s):
-    # Write your code here
-    pass
+    hour_24 = s[:-2]
+    hour, minute, second = hour_24.split(':')
+    hour = int(hour)
+    if s[-2:] == "PM":
+        if hour < 12:
+            hour = hour + 12
+        hour_24 = ':'.join([str(hour), minute, second])
+    else:
+        hour = hour%12
+        hour_24 = ':'.join([f'{hour:02d}', minute, second])
+    return hour_24
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     s = input()
 
     result = timeConversion(s)
 
-    fptr.write(result + '\n')
+    print(result)
+    # fptr.write(result + '\n')
 
-    fptr.close()
+    # fptr.close()
