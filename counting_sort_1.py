52069a8aff9c385485fac40c89cfc0c881b402a9
Abel Arismendy <a.arismendy@uniandes.edu.co>
2022-06-13 14:02:19 -0500

    counting sort 1

---
+++ counting_sort_1.py
@@ -0,0 +1,18 @@
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    res = [arr.count(i) for i in range(100)]
+    return res
+
+if __name__ == '__main__':
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
