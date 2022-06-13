80b91f6560263ead3a32381c0e8a930baed36fe5
Abel Arismendy <a.arismendy@uniandes.edu.co>
2022-06-13 16:49:14 -0500

    flipping the matrix

--- TEST_flipping_the_matrix.py
+++ TEST_flipping_the_matrix.py
@@ -6,7 +6,13 @@
 #
 
 def flippingMatrix(matrix):
-    pass
+    n = len(matrix)
+    s = 0
+    for i in range(n//2):
+        for j in range(n//2):
+            s += max(matrix[i][j], matrix[i][n-j-1], matrix[n-i-1][j], matrix[n-i-1][n-j-1])
+    return s
+
 
 if __name__ == '__main__':
 
