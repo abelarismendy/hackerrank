4216f98cc742066cf7acb42f49aba06a67aff4a1
Abel Arismendy <a.arismendy@uniandes.edu.co>
2022-06-14 19:43:33 -0500

    sales by match

---
+++ sales_by_match.py
@@ -0,0 +1,25 @@
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    pairs = 0
+    for color in set(ar):
+        pairs += (ar.count(color))//2
+    return pairs
+
+if __name__ == '__main__':
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    print(result)
