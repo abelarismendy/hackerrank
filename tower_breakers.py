f0342600f902d3a6e17e875342b9f4ad8d519e67
Abel Arismendy <39172024+abelarismendy@users.noreply.github.com>
2022-06-17 17:48:12 +0000

    tower  breakers

---
+++ tower_breakers.py
@@ -0,0 +1,29 @@
+
+#
+# Complete the 'towerBreakers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER m
+#
+
+def towerBreakers(n, m):
+    win = 1
+    if m == 1 or n%2==0:
+        win = 2
+    return win
+
+if __name__ == '__main__':
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        m = int(first_multiple_input[1])
+
+        result = towerBreakers(n, m)
+
+        print(result)
