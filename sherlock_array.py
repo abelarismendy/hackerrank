6799273ad4c3869b81cae2cdc7872a3234ceb4bc
Abel Arismendy <a.arismendy@uniandes.edu.co>
2022-06-25 18:32:34 -0500

    sherlock array

--- sherlock_array.py
+++ sherlock_array.py
@@ -6,21 +6,26 @@
 #
 
 def balancedSums(arr):
-    arr.sort()
+    # arr.sort()
     lo = 0
     hi = sum(arr)
     i = 1
+    print()
     if len(arr) == 1 or hi == 0:
         return 'YES'
     hi -= arr[0]
-    while lo < hi and i <= len(arr):
+    while i < len(arr):
+        if lo == hi:
+            return 'YES'
         lo += arr[i-1]
         hi -= arr[i]
-        print(i, lo, hi)
+        # print(i, lo, hi)
+        # print(arr[i-1], arr[i])
         if lo == hi:
             return 'YES'
         i += 1
     return 'NO'
+
 
 if __name__ == '__main__':
     T = int(input().strip())
