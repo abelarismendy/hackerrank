18c4b3725720ead01d2bad2cad52eaacf0552fc9
Abel Arismendy <abel@arismendy.co>
2022-07-06 07:13:18 -0500

    fix(bomberman.py): 5/25 cases pases

--- bomberman.py
+++ bomberman.py
@@ -13,14 +13,52 @@ import sys
 #  2. STRING_ARRAY grid
 #
 
+def printMatrix(matrix):
+    for i in matrix:
+        print(i)
+    print()
+
 def bomberMan(n, grid):
     for second in range(n):
         if second == 0:
-            counter_grid = [[0 for i in range(len(grid[0]))] for j in range(len(grid))]
-            print(counter_grid)
-        for i in grid:
-            for j in i:
-                pass
+            counter_grid = [[-1 if grid[i][j] == "." else 2 for j in range(len(grid[0]))] for i in range(len(grid))]
+            grid = [["." if grid[i][j] == "." else "O" for j in range(len(grid[0]))] for i in range(len(grid))]
+        else:
+            rows = len(grid)
+            cols = len(grid[0])
+            detonate = []
+            for i in range(len(counter_grid)):
+                for j, b in enumerate(counter_grid[i]):
+                    if b == -1 and (i,j) not in detonate and second % 2 == 0:
+                        counter_grid[i][j] = 2
+                        grid[i][j] = "O"
+                    elif b > 0:
+                        b -= 1
+                        counter_grid[i][j] = b
+                        grid[i][j] = "O"
+                    if b == 0:
+                        counter_grid[i][j] = -1
+                        grid[i][j] = "."
+                        if i > 0:
+                            counter_grid[i-1][j] = -1
+                            grid[i-1][j] = "."
+                        if j > 0:
+                            counter_grid[i][j-1] = -1
+                            grid[i][j-1] = "."
+                        if i + 1 < rows and counter_grid[i+1][j] != 1:
+                            grid[i+1][j] = "."
+                            counter_grid[i+1][j] = -1
+                            detonate.append((i+1, j))
+                        if j + 1 < cols and counter_grid[i][j+1] != 1:
+                            grid[i][j+1] = "."
+                            counter_grid[i][j+1] = -1
+                            detonate.append((i, j+1))
+        # printMatrix(matrix=counter_grid)
+        # printMatrix(matrix=grid)
+
+    # convert grid to list of strings
+    grid = [''.join(row) for row in grid]
+    return grid
 
 if __name__ == '__main__':
     first_multiple_input = input().rstrip().split()
